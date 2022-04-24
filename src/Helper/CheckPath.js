97b70dfe9aa136c77dfbf30439f34aa01802456a
Mohammed ALWahidi <nvs.apple01@gmail.com>
2022-04-24 16:05:24 +0300

    fix typo

--- src/Helper/CheckPath.js
+++ src/Helper/CheckPath.js
@@ -1,9 +1,12 @@
 function checkPath(location) {
-    const isAuth = JSON.parse(localStorage.getItem('auth'))
-    if((isAuth && location === '/account/login') || (isAuth && location === '/account/signup' ) || (isAuth && location === '/account/reset-password' ) ){
-       return true
-    }
-    
+  const isAuth = JSON.parse(localStorage.getItem("auth"));
+  if (
+    (isAuth && location === "/account/login") ||
+    (isAuth && location === "/account/signup") ||
+    (isAuth && location === "/account/reset-password")
+  ) {
+    return true;
+  }
 }
 
-export default checkPath
+export default checkPath;
